10817c8000ded10be4ed12aa862f3633a4fb201c
NilsLenaerts <nils.lenaerts00@gmail.com>
2021-11-28 15:01:14 +0100

    mooie homeknop

--- BaetenMaarten_LenaertsNils/resources/views/master.blade.php
+++ BaetenMaarten_LenaertsNils/resources/views/master.blade.php
@@ -9,6 +9,7 @@
             #homeButton{
                 text-decoration: none;
                 font-weight: 300;
+
             }
             #homeButton:link{
                 color: #000;
@@ -22,9 +23,23 @@
                 color: #333;
                 font-weight: 700;
             }
+
+            #homeButtonLogo:hover{
+                background: #ccc;
+                transition-duration: .5s;
+            }
+            #homeButtonLogo{
+                transition-duration: .5s;
+            }
+
         </style>
     </head>
     <body>
+        <div id=homeButtonLogo style="width: 50px;">
+            <a  href="/">
+                <img src="images/logo.png" alt='logo' style="width:50px;height:50px;">
+            </a>
+        </div>
         <h1><a id = "homeButton" href="/">D&D 5e</a></h1>
         <h3><a href = "login">Login</a></h3>
         <h2>@yield('subtitle')</h2>
