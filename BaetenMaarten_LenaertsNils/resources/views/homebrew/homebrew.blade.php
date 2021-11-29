f84880007197ba48749dd115372bcd1bc9dd3ebc
maartenbaeten509 <maartenbaeten509@users.noreply.github.com>
2021-11-29 15:17:23 +0100

    bugfixekes

--- BaetenMaarten_LenaertsNils/resources/views/homebrew/homebrew.blade.php
+++ BaetenMaarten_LenaertsNils/resources/views/homebrew/homebrew.blade.php
@@ -5,10 +5,9 @@
         <h2>Welcome to D&amp;D&nbsp; 5e homebrew creator</h2>
         <p>Want to add homebreww spells press this <a style="text-decoration: underline;" href="homebrewspellcreator">link</a></p>
         <p>Want see all the homebrew spells <p>
-        <form action="http://127.0.0.1:1200/searchSpell" method="post>
+        <form>
             <p>Level:  <input type="int" name="level" value="" id="level"/></p>  
-            <button type="submit"><strong>zoek via submit</strong></button> 
-            <button type="button" onclick="searchSpell()">
+            <button type="button" onclick="searchSpell()"><strong>Search</strong></button>
         </form>
         <p>&nbsp;</p>
         <p>&nbsp;</p>
