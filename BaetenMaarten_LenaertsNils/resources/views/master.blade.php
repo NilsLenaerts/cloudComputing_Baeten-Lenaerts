3092d57b7cd8fe0515e08e0eb6171164ad639d05
maartenbaeten509 <maartenbaeten509@users.noreply.github.com>
2021-11-29 16:22:53 +0100

    dropdown

--- BaetenMaarten_LenaertsNils/resources/views/master.blade.php
+++ BaetenMaarten_LenaertsNils/resources/views/master.blade.php
@@ -5,43 +5,84 @@
         <title>D&D 5e @yield('subtitle')</title>
         <script type="text/javascript" src="functions.js">
         </script>
-        <style type="text/css">
-            #homeButton{
-                text-decoration: none;
-                font-weight: 300;
-
-            }
-            #homeButton:link{
-                color: #000;
-                
-            }
-            #homeButton:visited {
-                color: #000;
-                
-            }
-            #homeButton:hover {
-                color: #333;
-                font-weight: 700;
+        <style>
+            ul {
+              list-style-type: none;
+              margin: 0;
+              padding: 0;
+              overflow: hidden;
+              background-color: #333;
             }
 
-            #homeButtonLogo:hover{
-                background: #ccc;
-                transition-duration: .5s;
-            }
-            #homeButtonLogo{
-                transition-duration: .5s;
+            li {
+              float: left;
             }
 
+            li a, .dropbtn {
+              display: inline-block;
+              color: white;
+              text-align: center;
+              padding: 14px 16px;
+              text-decoration: none;
+            }
+
+            li a:hover, .dropdown:hover .dropbtn {
+              background-color: red;
+            }
+
+            li.dropdown {
+              display: inline-block;
+            }
+
+            .dropdown-content {
+              display: none;
+              position: absolute;
+              background-color: #f9f9f9;
+              min-width: 200px;
+              box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+              z-index: 1;
+            }
+
+            .dropdown-content a {
+              color: black;
+              padding: 12px 16px;
+              text-decoration: none;
+              display: block;
+              text-align: left;
+            }
+
+            .dropdown-content a:hover {background-color: #f1f1f1;}
+
+            .dropdown:hover .dropdown-content {
+              display: block;
+            }
         </style>
+       
+    
     </head>
     <body>
-        <div id=homeButtonLogo style="width: 50px;">
-            <a  href="/">
-                <img src="images/logo.png" alt='logo' style="width:50px;height:50px;">
-            </a>
-        </div>
         <h1><a id = "homeButton" href="/">D&D 5e</a></h1>
-        <h3><a href = "login">Login</a></h3>
+        <ul>
+        <li>
+            <div id="homeButtonLogo" style="width: 50px;">
+                <a  href="/">
+                <img src="images/logo.png" alt='logo' style="width:50px;height:50px;">
+                </a>
+            </div>
+        </li>    
+        <li><a href="home">Home</a> </li>
+        <li><a href="login">Login</a></li>
+        <li><a href="homebrew">Homebrew</a></li>
+        <li class="dropdown">
+          <a href="javascript:void(0)" class="dropbtn">Rules</a>
+          <div class="dropdown-content">
+            <a href="spells">Spells</a>
+            <a href="foundClasses">Classes</a>
+            <a href="">Races</a>
+          </div>
+        </li>
+        <li><a href="calendar">Calendar</a></li>
+        </ul>
         <h2>@yield('subtitle')</h2>
         @yield('content')
         
