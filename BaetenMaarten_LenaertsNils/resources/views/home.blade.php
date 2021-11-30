db0374ea24f6ee6660f4ee1c432fb616f54977b1
maartenbaeten509 <maartenbaeten509@users.noreply.github.com>
2021-11-30 15:10:10 +0100

    home

--- BaetenMaarten_LenaertsNils/resources/views/home.blade.php
+++ BaetenMaarten_LenaertsNils/resources/views/home.blade.php
@@ -9,11 +9,6 @@
     </head>
     <body>
         <h2>Welcome to D&amp;D&nbsp;</h2>
-        <p><a style="text-decoration: underline;" href="calendar">gameCalendar</a></p>
-        <p>For random character creation press this <a style="text-decoration: underline;" href="randomCharacterCreation">link</a></p>
-        <p>For random dungeon creation press this <a style="text-decoration: underline;" href="google.com">link</a></p>
-        <p>For all D&amp;D rules press this <a style="text-decoration: underline;" href="rules">link</a></p>
-        <p>For all D&amp;D homebrew content press this <a style="text-decoration: underline;" href="homebrew">link</a></p>
         <p><img src="https://dnd.wizards.com/sites/all/themes/dx/logo.png" width="382" height="64" /></p>
         <p><em>&nbsp;May the die be in your favor</em></p>
         <p>&nbsp;</p>
