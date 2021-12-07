20e15fcef6732debe0b27f47cbe93075c9c08b73
Nils Lenaerts <nils.lenaerts00@gmail.com>
2021-12-07 13:13:04 +0100

    weg met de unorderd list

--- BaetenMaarten_LenaertsNils/resources/views/rules/sections.blade.php
+++ BaetenMaarten_LenaertsNils/resources/views/rules/sections.blade.php
@@ -7,15 +7,15 @@
     <ul>
         @foreach ($subSections as $section)
 
-        <li>
+        <li style="list-style-type:none">
             <details class="details-example">
                 <summary><strong>{{$section->name}}</strong></summary>
                 <ul class="no-bullets">
-                    <li>
+                    
                         <!-- display dan de desc of time -->
                         <div id="{{$section->index}}"></div>
                         <script>getDescription("{{$section->index}}");</script>
-                    </li>
+                    
                 </ul>
             </details>
         </li>
