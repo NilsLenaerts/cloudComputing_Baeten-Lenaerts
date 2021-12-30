36c423419c069e56e232e62cf82e03cd09a6fd4a
NilsLenaerts <nils.lenaerts00@gmail.com>
2021-12-30 18:41:58 +0100

    twerkt half

---
+++ BaetenMaarten_LenaertsNils/app/Soap/getEventDTORequest.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Soap;
+
+class getEventDTORequest
+{
+    //protected $id;
+    protected $EventDTO;
+
+    public function __construct($id){
+        //$this->id = $id;
+    }
+
+    public function get(){
+        return $EventDTO;
+    }
+}
